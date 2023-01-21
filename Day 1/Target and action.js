daf60501efad362e6c80addf14df870a7f6b7c7c
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-20 22:46:55 -0500

    Create Target and action.js

---
+++ Day 1/Target and action.js
@@ -0,0 +1,45 @@
+/// 1. What character alerts the program that jQuery code follows?
+
+$("target").method();
+
+/// 2. Type the characters that comprise the jQuery selector in the statement below.
+
+//("img")
+
+$("img").hide();
+
+/// 3. Type the characters that comprise the jQuery method in the code below. (Don't type the dot and semicolon.)
+
+//.hide
+
+$("img").hide();
+
+/// 4. Type the character that separates the selector from the method
+
+//.
+
+$("p").hide();
+
+///7. Hide all paragraphs.
+
+$("p").hide();
+
+/// 8. Code the selector for all h2 headings.
+
+$("h2");
+
+/// 9. Code the selector for an element of your choice.
+
+$("a").hide();
+
+/// 10. A method you haven't learned yet is show(), the opposite of hide(). Code a statement that shows all images.
+
+$("img").show();
+
+/// Code the selector that selects all h3 headings and all paragraphs.
+
+$("h3", "p");
+
+/// 12. Select any three elements of your choice and hide them.
+
+$("a", "p", "h1").hide();
