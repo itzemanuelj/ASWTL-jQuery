c075e5f83c6bb66b48ae6578c381479e988d1858
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-20 23:19:12 -0500

    created addclass

---
+++ Day 2/Add a class.js
@@ -0,0 +1,39 @@
+/// 1. In the box on the right, type the method for adding a class, including the parentheses.
+
+$("p").AddClass("class");
+
+/// 3. You're adding the class "pretty" to all paragraphs. Fill in the blank.
+
+$("p").AddClass("pretty");
+
+/// 4. You're adding the class "pretty" to all paragraphs. Type the rest of the statement.
+
+$("p").AddClass("pretty");
+
+$("h2").AddClass("main");
+
+$("div, p").addClass("emphasized");
+
+/// 7. Add the classes "formal" and "tinted" to all tables.
+
+$("table").addClass("formal, tinted");
+
+/// 8. Add the class "fancy" to all unordered lists.
+
+$("ul").addClass("fancy");
+
+/// Add the class "centered" to all images.
+
+$("img").addClass("centered");
+
+/// 10. Add a class to all divs. Make up the class name.
+
+$("div").addClass("random-class");
+
+/// Remove the class "dark" from all instances of a particular kind of element, like all paragraphs. You choose the kind of element.
+
+$("p").removeClass("random-class");
+
+/// 12. Remove two classes from two elements of your choice. Make up the class names.
+
+$("p, h2").removeClass("random-class");
