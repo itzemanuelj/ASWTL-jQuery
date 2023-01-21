b879d8d5791d7da6ae0497ed4b69ad64a21c720c
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-21 14:48:07 -0500

    added files to day 4
    selecting id

---
+++ Day 4/Select by id.js
@@ -0,0 +1,44 @@
+// Select by id
+
+/// 1. To select by ID, fill in the blank.
+
+$("p#intro").addClass("big");
+
+/// 2. To select an image with the ID of "product", fill in the blank.
+
+$("img#product").addClass("main");
+
+/// 3. To insert the text "Hi," fill in the blank.
+
+$("p#greeting").text("Hi");
+
+$("p.greeting").text("hello world");
+
+$("h4#rules").text("Read Carefully");
+
+/// 6. Select a list item with an ID of "i2".
+
+$("li#i2");
+
+/// ill in the blank to insert an h4 heading that says "Hi".
+
+$("div#intro").html("<h4>Hi</h4>");
+
+/// 8. Select the paragraph with an ID of "intro".
+
+$("p#intro");
+
+/// 9. Fill in the blank to insert the text "Hello world".
+
+$("h2#greeting").text("Hello world");
+
+/// 10. Insert HTML of your choice in the paragraph with an ID of "top".
+
+$("p.#top").html("<string>loremisulolmsjbs shbshsb shgvshys </string>");
+
+/// 11. Insert text of your choice into an h2 heading that has an ID of your choice.
+
+$("h2#main").text("hello");
+
+/// 12. Insert HTML of your choice into an element of your choice with an ID of your choice.
+$("p#main").html("<p> lorem </p>");
