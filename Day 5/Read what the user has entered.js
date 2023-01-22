4c13f23c40b5418695a60f682dd67bc634b9db7e
Emanuel Joness <itzemanuelj@gmail.com>
2023-01-22 00:24:44 -0500

    mvp commit for Read what the user has entered
    jQuery

---
+++ Day 5/Read what the user has entered.js
@@ -0,0 +1,44 @@
+/// Read what the user has entered
+
+/// 1. The user's input is stored in a ______.
+
+var let;
+
+/// 2. What is the keyword for declaring a variable?
+
+// let
+
+/// 3. Fill in the blank to read and store the user's email address entry. The ID of the field is "mail".
+
+let whatUserEntered = $("input#mail").val();
+
+/// 4. Fill in the blank to read and store the user's email address entry.
+
+let whatTheUserEntered = $("input#mail").val();
+
+let whatTheUserHasEntered = $("form.login").val();
+
+/// 6. Read the user's entry in the field with an ID of "name". Store it in the variable monike
+
+let moinke = $("input#name").val();
+
+/// 7. Read the user's entry in the field with an ID of "f1" and store it in the variable prof.
+
+let prof = $("input#f1").val();
+
+/// 9. You're storing the user's entry in the variable city. The ID of the field is "muni". Code everything before the dot.
+
+let city = $("input#muni").val();
+
+/// 10. You're storing the user's entry in the variable city1. The ID of the field is "muni". Code the entire statement.
+
+let city1 = $("inpur#muni").val();
+
+/// 11. You're storing the user's entry in a variable of your choice. The ID of the field is "firstName". Code the entire statement.
+
+let choice = $("inpt#firstName").val();
+
+/// 12. You're storing the user's entry in a variable of your choice. The ID of the field is also your choice. Code the entire statement.
+
+let teamName = $("Detroit#Pistons").val();
+console.log(teamName);
